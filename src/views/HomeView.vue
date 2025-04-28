8ee8ebc4b24405f774a8d02c2e5d79c3631b0310
DorotaOrkisz <d_orotka@wp.pl>
2025-04-28 19:09:35 +0200

    #4 init change

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,21 @@
 <template>
+  <div class="home-view">
+  <h1>THE BEST GROCERY FOOD</h1>
   <ProductList :products="store.products" />
+  </div>
 </template>
+
+<style lang="scss">
+.home-view{
+  background: #ffffcc;
+
+  h1{
+  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+  font-size: 400;
+  color: #cc0000
+  }
+}
+</style>
 
 <script setup>
 import { use_product_store } from '@/store/product_store'
