8bbef2caa333d808298bc30e13aa038f58b3c236
apietryga <antek.pietryga@gmail.com>
2025-04-28 17:46:20 +0200

    #2 init

---
+++ src/views/AboutView.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="about">
+
+    Tutaj już możesz pisać HTML'em
+
+  </div>
+</template>
+
+<style lang="scss" scoped>
+// tutaj możesz pisać scss
+.about{
+  border:2px dashed red;
+}
+</style>
